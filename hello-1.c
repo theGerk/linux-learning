2d963cf8676c342352184d3a670bdd63197dfa72
Benji <benji@altmansoftwaredesign.com>
2021-07-07 12:20:06 -0500

    hello world

---
+++ hello-1.c
@@ -0,0 +1,17 @@
+#include <linux/module.h>      // Needed by all modules
+#include <linux/kernel.h>       // Needed for KERN_INFO
+
+
+int init_module()
+{
+	printk(KERN_INFO "Hello world");
+
+	return 0;
+}
+
+
+void cleanup_module()
+{
+	printk(KERN_INFO "Goodbye world\n");
+}
+
