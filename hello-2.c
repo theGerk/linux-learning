2d3581167cb6cc38fa21b163dec862d364efea2a
Benji <benji@altmansoftwaredesign.com>
2021-07-08 18:21:00 -0500

    hello world 2

---
+++ hello-2.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>     // Needed by all modules
+#include <linux/kernel.h>     // Needed for KERN_INFO
+#include <linux/init.h>       // Needed for the macros: module_init() and module_exit()
+
+
+static int __init hello_2_init()
+{
+    printk(KERN_INFO "Hello world 2\n");
+    return 0;
+}
+
+static void __exit hello_2_exit()
+{
+    printk(KERN_INFO "Goodbye world 2\n");
+}
+
+module_init(hello_2_init);
+module_exit(hello_2_exit);
