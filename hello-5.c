b081dc0b01ca4dff4e180a4fd0bd7ab4516e44a3
Benji <benji@altmansoftwaredesign.com>
2021-07-09 13:04:05 -0500

    hello 5 not working

--- hello-5.c
+++ hello-5.c
@@ -4,15 +4,13 @@
 #include <linux/init.h>
 #include <linux/stat.h>
 
-#define printinf(x) printk( KERN_INFO x )
-
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Benji Altman");
 
-static short int myshort = 1;
+static short myshort = 1;
 static int myint = 420;
-static long int mylong = 9999;
+static long mylong = 9999;
 static char* mystring = "blah";
 static int myintArray[2] = { -1, -1, };
 static int arr_argc = 0;
@@ -49,13 +47,22 @@ MODULE_PARM_DESC(myintArray, "An array of integers");
 static int __init hello_5_init(void)
 {
     int i;
-    printinf("hello world\n");
+    printk(KERN_INFO "hello world 5\n===========\n");
+    printk(KERN_INFO "myshort is a short integer: %hd\n", myshort);
+    printk(KERN_INFO "myint is an integer: %d\n", myint);
+    printk(KERN_INFO "mylong is a long integer: %ld\n", mylong);
+    printk(KERN_INFO "mystring is a string: %s\n", mystring);
+    for(i = 0; i < (sizeof myintArray / sizeof(int)); i++)
+    {
+        printk(KERN_INFO "my int array[%d] = %d\n", i , myintArray[i]);
+    }
+    printk(KERN_INFO "got %d arguments for myintArray.\n", arr_argc);
     return 0;
 }
 
 static void __exit hello_5_exit(void)
 {
-    printinf("goodbye world\n");
+    printk(KERN_INFO "goodbye world\n");
 }
 
 
